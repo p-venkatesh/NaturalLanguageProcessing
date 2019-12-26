bfb04ae408400cbf0653bbbdb2c7ec524a2bc572
P.Venkatesh <44500431+p-venkatesh@users.noreply.github.com>
2019-12-26 11:33:23 +0530

    Update natural_language_processing.py

--- natural_language_processing.py
+++ natural_language_processing.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+dataset = pd.read_csv('Restaurant_Reviews.tsv')
 
 # Cleaning the texts
 import re
@@ -16,7 +16,7 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 corpus = []
 for i in range(0, 1000):
-    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = re.sub('[^a-zA-Z]', dataset['Review'][i])
     review = review.lower()
     review = review.split()
     ps = PorterStemmer()
